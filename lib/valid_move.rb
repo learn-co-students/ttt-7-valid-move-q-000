cfb87254ac93918cbbabc97c11cfff526a342c4a
Krishna Vempati <krishna.vempati@versapay.com>
2015-10-13 20:30:53 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  index = position.to_i
+  index.between?(1,9) && !position_taken?(board,position)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  index = position.to_i
+  board[index-1] == "X" || board[index-1] == "O"
+end
+
