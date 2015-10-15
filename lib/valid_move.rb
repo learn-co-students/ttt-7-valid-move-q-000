d6b7800a4f83eb7de265527d2a2d230f3bb1f7c0
zachnewburgh <zach.newburgh@gmail.com>
2015-10-15 11:47:51 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  position = position.to_i-1
-  if position.between?(0,8) && position_taken?(board,position) == false
+  # turn position into integer, but use (1,9) because user is asked for 1-9 (between?(1,9)).
+  position = position.to_i
+  if position.between?(1,9) && position_taken?(board,position) == false
     true
   else
     false
@@ -10,6 +11,8 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
+  # account for positions in array (position - 1)
+  position = position - 1
   if board[position] == " " || board[position] == "" || board[position] == nil
     false
   elsif board[position] == "X" || board[position] == "O"
