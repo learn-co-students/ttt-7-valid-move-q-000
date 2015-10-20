b7878256765dbdb3f810c246a1ad805394f86c7f
eaylward8 <eaylward8@gmail.com>
2015-10-20 10:44:48 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  #tests in valid_move_spec.rb give position as a String ("1)")
+  #use to_i method to convert to integers
+  if position.to_i >= 1 && position.to_i <= 9 && !position_taken?(board, position.to_i)
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, position)
+  #use position - 1 to account for array indices vs human perception of tic tac toe spaces
+  if board[position - 1] == "X" || board[position - 1] == "O"
+    return true
+  else
+    return false
+  end
+end
