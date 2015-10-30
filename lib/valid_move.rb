a4bafeec7a82ce4c8bc28ee9d02efd2ac02e9a6a
anorico <allan.norico@gmail.com>
2015-10-29 21:47:51 -0400

    Done

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
+def valid_move?(board,position)
+  position = position.to_i #convert string to integer
 
+  if position >= 0 and position < 9 #check for legal position
+    if board[position-1].strip.empty?
+      return true
+    else
+      return false #position is occupied
+    end
+  else
+    return false #illegal position
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
