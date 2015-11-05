16ce7685feca4a3e718f463bbe23e2e18cd46f38
tyrantdavis <davistyrant@gmail.com>
2015-11-05 15:50:24 -0600

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,27 @@
-# code your #valid_move? method here
+# def valid_move?(board, position)
+#    if the position is on the board is empty return true else return false 
+# if position_taken? ==" "
+#   return true
 
+#     if valid_move == " "
+#     if the position is not on the board return false  return false
+#   else
+#    return true
+#   end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+#   space = board[position].to_i
+#    if position_taken?(space) == false && space.between(1, 9) == false
+#     return false
+#   else 
+#     return true
+#   end
+# end
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+    position = "5"
+def valid_move?(board, position)
+  if position.to_i.between?(1,9) && " " == board[position.to_i - 1]
+    return true 
+  else 
+    return false
+  end
+end
