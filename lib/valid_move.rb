3d49fceb7727023255c85a7f19a2a3b129601d2c
pajamaw <prw42@cornell.edu>
2015-11-11 17:33:20 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,15 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, location)
+  if location.to_i == 1..9
+    return true
+  else
+    return false
+  end
+    if position_taken? == true
+      return false
+    else
+      return true
+    end
+end
