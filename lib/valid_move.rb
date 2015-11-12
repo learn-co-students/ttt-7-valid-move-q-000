d9925454c1ea6238f26e03ff3953d463c021c0d1
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-12 14:29:02 +0000

    nitrous-backup at Thu Nov 12 14:29:02 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,6 @@
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 position = "5"
-# position = pos1.to_i - 1
 
 def position_taken?(board,position)
   taken = nil
@@ -11,17 +10,16 @@ def position_taken?(board,position)
   elsif board[position] == "X" || board[position] == "O"
     taken = true
    end
-#  puts taken
   taken
 end
 
 def valid_move?(board, position)
   taken1 = nil
   if position.to_i.between?(1,9)
-    if !position_taken?(board,position.to_i-1)
-      taken1 = true
-  else
+    if position_taken?(board,position.to_i-1)
       taken1 = false
+  else
+      taken1 = true
     end
   end
   taken1
