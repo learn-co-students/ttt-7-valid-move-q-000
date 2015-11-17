da60fec1f71c7c808eb7c882029c73da973bb0e4
jlee0910 <jlee0910@gmail.com>
2015-11-17 14:38:33 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,19 +1,35 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-if board[position.to_i - 1] == " "
-  return true
-else
-  return false
-end
+  position = position.to_i
+  if position == 1..9 || position_taken?
+    return true
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, position)
- board[position] != " "
- position == nil 
- if board[position] == "X" || board[position] == "O"
-      return true
-   else
-      return false
-   end
- end
+#def position_taken?(board, position)
+ #board[position] != " "
+ #position == nil 
+ #if board[position] == "X" || board[position] == "O"
+  #    return true
+   #else
+    #  return false
+   #end
+ #end
+
+ def position_taken?(board, position)
+  # basic solution: 
+
+  # taken = nil
+  # if (board[position] ==  " " || board[position] == "" || board[position] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  (board[position] ==  " " || board[position] == "" || board[position] == nil) ? 
+    false : true
+
+end
