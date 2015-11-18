a9e178b7786f22a7482df5ff308e6ff0ecb43a4f
lifeiscontent <aaron@lifeiscontent.net>
2015-11-18 14:32:07 -0800

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  board.length - position.to_i <= 0 ? nil : !position_taken?(board, position.to_i)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  puts board[position - 1].class
+  !["", " ", nil].include?(board[position - 1])
+end
