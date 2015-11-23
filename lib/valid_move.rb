6a0c8226fc53f999f7a0601b0aac51c00673e730
corya0687 <corya0687@gmail.com>
2015-11-23 22:11:39 +0000

    done

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
-# code your #valid_move? method here
+require_relative "./position_taken7.rb"
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, position)
+position= position.to_i - 1
+if !(position_taken?(board, position))
+
+
+    if position.between?(0,8)
+     return true
+    elsif position < 0 || position > 8
+      return false
+
+
+    end
+
+end
+
+end
+
+
