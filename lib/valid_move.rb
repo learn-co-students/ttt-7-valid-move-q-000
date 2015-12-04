776196e6f399befff2a90849d4074c9312eefaa4
phamducv <phamducv@gmail.com>
2015-12-04 20:59:30 +0000

    nitrous-backup at Fri Dec  4 20:59:30 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move?(board, position)
-        position_taken?(board, position.to_i-1) && position.to_i.between?(1,9)
+        position_taken?(board, position.to_i-1) && position.to_i.between?(0,8)
 
 
 end
