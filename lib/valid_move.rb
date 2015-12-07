5920f8429b67ce8858a336df2325fdedc21b4c9f
reifnotreef <bridgerrhammond@gmail.com>
2015-12-07 20:03:17 +0000

    nitrous-backup at Mon Dec  7 20:03:16 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  if !position_taken?(board, position) && position.to_i.between?(1, 9)
-  true
-  end
+  return true if !position_taken?(board, position) && position.to_i.between?(1, 9)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position) # pull user input/board
-  return false if board[position] == " " # False if the position is empty
-  return false if board[position] == "" # False if the position is edge
+  return false if board[position] == " " || board[position] == ""
   return true if board[position] == "X" || board[position] == "O"
 end
 
