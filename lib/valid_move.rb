d46f36c2ab01af4b121d558acb98434a75c2f03e
Richard-Frost <frost.rchrd@gmail.com>
2015-12-07 11:00:24 +0000

    nitrous-backup at Mon Dec  7 11:00:24 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,16 @@
 def position_taken?(board, position)
 
-  if board[position.to_i - 1] == " " || board[position.to_i - 1] == "" || board[position.to_i - 1] == nil
+  if board[position] == " " || board[position] == "" || board[position] == nil
     return false
-  elsif board[position.to_i - 1] == "X" || board[position.to_i - 1] == "O"
+  elsif board[position] == "X" || board[position] == "O"
     return true
   end
 end
+
 def valid_move?(board, position)
   current_move = position.to_i - 1
 
-  if !position_taken?(board, position) && current_move.between?(0, 8)
+  if !position_taken?(board, current_move) && current_move.between?(0, 8)
     return true
   else
     false
