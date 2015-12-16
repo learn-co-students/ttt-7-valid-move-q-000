aea08779274cf6433779518ea3bc56a1f285f255
jaredwerba <jwerba@gmail.com>
2015-12-16 17:14:41 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,12 @@
 # code your #valid_move? method here
-def valid_move?(board, position)?
+def valid_move?(board, position)
   if board[position] == 0..8
-    return true
-    elsif board[position] == "X" || board[position] == "O"
-    return false
-    else
-    board[position].is_a? Integer
-    return true
+#     return true
+#     elsif board[position] == "X" || board[position] == "O"
+#     return false
+#     else
+#     board[position].is_a? Integer
+#     return true
   end
 end
 
