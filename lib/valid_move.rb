74e31b10b3cff217030a953022440da3e4f0f709
chintas1 <chintas1@tcnj.edu>
2015-12-18 17:05:37 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
-
+def valid_move?(board, pos)
+  pos = pos.to_i
+  if(pos.between?(1,9) && !position_taken?(board, pos-1))
+    valid = true
+  else
+    valid = false
+  end
+  valid
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, pos)
+  if board[pos]=="X" || board[pos]=="O"
+    taken = true
+  else
+    taken = false
+  end
+  taken
+end
