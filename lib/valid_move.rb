19a7baa8f9dcc336b8106f8218b32075993e0cb8
sigkapphi <sigkapphi@gmail.com>
2015-12-23 17:24:34 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,3 +18,4 @@ end
 #position.to.i.between and position.between both work for  returns true for a valid position on an empty board
 #since we are calling upon position_taken? method , we have to remember to pass the same arguments through it as valid_move?
 #also, since we are calling upon a previous method in position_taken? in valid_move?, there is no need to have to manipulate board[location.to_i-1] in valid_move?, as it is already done in position_taken?.
+#simplified def position_taken? to one line, using not equal boolean instead of have 3 if,elsif,else statements meaning same thing.
