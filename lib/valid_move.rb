d0f57743ddc19ba5f761a057a6cce4ca98a8d1e9
ptcloutier <cloutier.perrin@gmail.com>
2015-12-29 16:06:52 +0000

    nitrous-backup at Tue Dec 29 16:06:52 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board,position)
+    if !position_taken?[board, position.to_i] && [position.to_i-1].between?(0,9)
+   return true
+    else position_taken?[board, position.to_i]
+      return false
+end
+end
