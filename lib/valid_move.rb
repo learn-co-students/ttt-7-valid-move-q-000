e2b6b9f21071370bd8bfad0ea07b9ae16d5f17d8
the-widget <brennenawana108ny@gmail.com>
2016-01-18 19:18:25 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,13 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+def position_taken?(board, position)
+  board[position] != " " && board[position] != "" && board[position] != nil
+end
+
 def valid_move?(board, position)
   if position_taken?(board, position.to_i-1) == true
     return false
   elsif board[position.to_i-1] == " "
     true
   end
-end
-
-def position_taken?(board, position)
-  board[position] != " " && board[position] != "" && board[position] != nil
 end
