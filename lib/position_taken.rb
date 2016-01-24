bd4d8fecc4930f08f1a3810dc66b3583e7c4324c
flora jha <florajha@gmail.com>
2016-01-24 21:44:07 +0000

    Done.

---
+++ lib/position_taken.rb
@@ -0,0 +1,10 @@
+# code your #position_taken? method here!
+board =[" "," "," "," "," "," "," "," "," "]
+def position_taken?(board , position)
+  if board[position] == " " || board [position] == "" || board [position] ==nil
+    return false
+  end
+else
+  return true
+
+end
