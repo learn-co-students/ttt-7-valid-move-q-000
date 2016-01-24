8713a3eff89536bbd5dae7f53b98b6d1afab3908
michaeljohnorlando <michael.john.orlando@gmail.com>
2016-01-24 03:16:39 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
-# code your #valid_move? method here
 
+def position_taken?(board,position)
+  if   board[position] == "X" || board[position] == "O"
+     true
+  else board[position] == " " || board[position] ==  "" || board[position] ==  nil
+     false
+  end
+end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board,position)
+  position = position.to_i - 1
+  if    position_taken?(board,position) == true || position.between?(1, 9) == false && board[position] != " "
+      false
+  else  position_taken?(board,position) == false && position.between?(1, 9) == true
+      true
+  end
+end
+
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    position = "1"
+valid_move?(board,position)
+
+# no space is allowed
+# position_taken? (board,position)
+#                ^
+#http://stackoverflow.com/questions/8541733/syntax-error-unexpected-expecting
