864de602d3afc4ab123e6a815f4016378d603384
fromlisa <sen4532@g.rit.edu>
2016-01-31 07:35:00 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
+def valid_move?(array, number)
+  position = number.to_i - 1
+  if position_taken?(array, position) == true
+    false
+  elsif position_taken?(array, position) == false
+    if position.between?(0,8)
+      true
+    end
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array, number)
+  if array[number] == " "
+    false
+  elsif array[number] == nil
+    false
+  else
+    true
+  end
+end
+
+
