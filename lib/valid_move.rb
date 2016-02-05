a486e70c33f77331fd233590311b905f2e00818d
zaynrahn <zaynrahn@gmail.com>
2016-02-05 21:42:14 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,14 @@
+#require_relative './ttt-6-position-taken-rb-q-000/position_taken.rb'
 # code your #valid_move? method here
 def valid_move?(board,position)
-  if board[position.to_i-1] == " "
-    true
-#  if position_taken?(board,position) == false || position_taken?(board,position)==nil
-#    true
-  end
+  board[position.to_i-1] == " "
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  if (board[position.to_i]==' ')||(board[position.to_i]=='')||(board[position.to_i]==nil)
+    false
+  elsif (board[position.to_i]=='X')||(board[position.to_i]=='O')
+    true
+  end
+end
