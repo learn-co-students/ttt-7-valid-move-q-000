b9c43cd81037aa584a09123cb193f7c00bc623dd
jamesmccarthy65 <jamesmccarthy65@gmail.com>
2016-02-08 23:25:03 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,12 @@
-
-board = [" ", " ", " ", " ", " ", " ", "X", " ", " "]
-def position_taken?(board, position)
-  if board[position] == " " || board[position] == ""
-    puts " true"
-    return true
-  elsif board[position] == nil
-    return true
-  elsif board[position] == ("X"||"O")
-    return false #returns false if the position is occupied
-  end
-end
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, position)
-  if position.to_i.between?(0, 9) || position_taken?(board, position.to_i) != true
+  true_pos = board[(position.to_i)-1]
+
+  if true_pos != "X" && true_pos != "O" && position.to_i.between?(0, 9)
     return true
-  elsif (position.to_i.between?(0, 9) == true && position_taken?(board, position.to_i) != true)
-    return false
   else
     return false
   end
 end
 
-valid_move?(board, 6)
