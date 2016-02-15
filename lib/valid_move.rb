4764f82f47ce2b3c3d95a9c9bd239625e5b93759
bradleyoashburn <bradleyoashburn@gmail.com>
2016-02-14 23:17:33 -1000

    all tests passing

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
 # code your #valid_move? method here
-
+def valid_move?(board, user_input)
+  user_input = user_input.to_i
+  if user_input > 0 && user_input < 10
+    if position_taken?(board, user_input) == false
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  if board[position-1] == ' '
+    return false
+  elsif board[position-1] == ''
+    return false
+  elsif board[position-1] == nil
+    return false
+  elsif board[position-1] == 'X' || board[position-1] == 'O'
+    return true
+  end
+end
