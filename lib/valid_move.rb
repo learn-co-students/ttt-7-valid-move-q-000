a5447cd1e828e9af278be7436ecc041e60771f6b
ajmcg <a.j.mccormickgonzales@gmail.com>
2016-02-19 23:26:35 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,7 @@
 def valid_move?(board, position)
         if position_taken?(board, position.to_i-1) == false && position.to_i.between?(1,9)
             true
-        else
-            position_taken?(board, position.to_i-1) == true && position.to_i.between?(1,9)
+        else position_taken?(board, position.to_i-1) == true && position.to_i.between?(1,9)
             false
         end
     end
@@ -15,7 +14,7 @@ def position_taken?(board,position)
     false
     elsif board[position] == nil
     false
-    else   board[position] == "X" || "O"
+    else  board[position] == "X" || "O"
     true
   end
 end
