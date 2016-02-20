2eab84a6d030f04214efe17f9cbe2c969fd9198d
TracySpitler <tracyspitler@gmail.com>
2016-02-20 23:05:32 +0000

    nitrous-backup at Sat Feb 20 23:05:32 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-  if !position_taken?(board, position.to_i-1)
+  if position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
       true
-    end
   end
 end
 
