e21633515f36158c2808b0e5a073ecc39929f914
neilmryan <neilmarkoliverryan@gmail.com>
2016-02-21 22:52:21 +0000

    nitrous-backup at Sun Feb 21 22:52:21 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,3 +15,10 @@ def position_taken?(board, int)
     false
   end
 end
+
+def valid_move?(board, position)
+  position = position.to_i - 1
+  if position.between?(0, 8) == true && position_taken?(board, position) == false
+    true
+  end
+end
