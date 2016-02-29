ec1bf11a4f5cee506aa2e213f6a9a2637e031dec
jonpstone <jon_p_stone@yahoo.com>
2016-02-29 12:31:11 +0000

    nitrous-backup at Mon Feb 29 12:31:10 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,15 @@
 def valid_move?(board, position)
   if position_taken?(board, position.to_i-1) == false && position.to_i.between?(1, 9)
-    return true
+    true
     else
-    return false
+    false
   end
 end
 
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
-    return false
+    false
   elsif board[position] == "X" || board[position] == "O"
-    return true
+    true
   end
 end
