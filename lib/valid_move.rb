1c63562ff8021b406b0a8bf44d4c5074ecbeb4aa
Larry Eisenstein <larryeisenstein@gmail.com>
2016-03-09 13:59:16 +0000

    nitrous-backup at Wed Mar  9 13:59:16 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,6 @@
 def valid_move?(board, position)
   position = position.to_i - 1
   return position.between?(0,8) && !position_taken?(board, position)
-
-
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
