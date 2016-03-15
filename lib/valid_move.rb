1992e0c8b01d6b2a2bac27b36a6aff9df48e0119
Jonathan Chin <Jonathankchin@gmail.com>
2016-03-15 23:57:06 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
+def valid_move?(board, position)
+  x = position.to_i #need to convert the input to an integer
 
+  if x.between?(1,9)
+    if position_taken?(board, x-1) == false
+      true
+    elsif position_taken?(board, x-1) == true
+      false
+    end
+  else
+    false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  if board[position] == " " || board[position] == ""
+    false
+  elsif board[position] == nil
+    false
+  elsif board[position] == "X" || board[position] == "O"
+    true
+  end
+end
