b0c749f830890dfb64c51fde255326a734d2245b
einarstensson <einar.stensson@gmail.com>
2016-03-15 23:23:14 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,4 @@
 def valid_move?(board, position)
-  p board
-  puts position
-  puts position_taken?(board, position)
   return true unless position.to_i > (board.length + 1) ||
                      position_taken?(board, position) == true 
   false
