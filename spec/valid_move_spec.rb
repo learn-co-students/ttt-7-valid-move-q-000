9340fe7325bee27b37453146eebb4ded86212144
NickSmithNYC <nicksmith1986@gmail.com>
2016-03-21 14:44:03 +0000

    Done.

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -1,7 +1,6 @@
 require_relative "../lib/valid_move.rb"
 
 describe './lib/valid_move.rb' do
-  # Needs more specs
 
   it 'returns true for a valid position on an empty board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
@@ -12,11 +11,11 @@ describe './lib/valid_move.rb' do
   it 'returns nil or false for an occupied position' do
     board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
     position = "5"
-    
+
     expect(valid_move?(board, position)).to be_falsey
   end
 
-  it 'returns nil or false for a position that is not on the board' do 
+  it 'returns nil or false for a position that is not on the board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     position = 100
 
