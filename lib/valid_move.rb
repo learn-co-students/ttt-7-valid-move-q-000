185cabeaf9a66fd6b27ab22b997a7ab8160cdb84
ndaniele <n.daniele@gmail.com>
2016-03-22 17:37:24 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 def valid_move?(board,position)
+  position = position.to_i
+  (position.between?(1, 9) && !position_taken?(board, position-1))
 end
 
 def position_taken?(board,position)
-  (board[position] != " " && board[position] != "" && board[position] != nil)
+  (board[position] == "X" || board[position] == "O")
 end
-
