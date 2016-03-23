7dc91cbd1e168994151f0287a7aeb7c88b8175a9
jd2rogers2 <jd2rogers2@gmail.com>
2016-03-23 09:51:47 -0400

    donzo

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
 
+def valid_move?(board, position)
+  if position_taken?(board, position.to_i) == false && [1, 2, 3, 4, 5, 6, 7, 8, 9].include?(position.to_i)
+    true
+  else
+  end 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, position)
+  if board[position - 1] == "X" || board[position - 1] == "O"
+    true
+  else
+    false
+  end
+end
+
+
