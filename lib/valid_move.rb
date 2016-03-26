b2b79275d4a22460fe0f8440d9676f00d6641263
Kia <kiawhitehead@gmail.com>
2016-03-26 20:30:43 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
-# code your #valid_move? method here
+def valid_move?(board,position)
+  move = position.to_i - 1 #they put in a number and I subtract 1 for the array and set this input to variable move
+  if move.between?(0,8) && !position_taken?(board, move) #if the variable move is between 0-8 AND position_taken? is false then it is a valid move. I pass variable move in the argument instead of position because it has been validated
+    true
+  end
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+
+def position_taken? (board,position)
+   if board[position] == " " ||
+      board[position] == ""  ||
+      board[position] == nil
+      false
+    else
+      true
+  end
+end
