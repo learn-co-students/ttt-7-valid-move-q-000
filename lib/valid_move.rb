51388d1d18fbfc271778550635992384f9c750a4
Llubomir Stanojlovic <candid.llama@gmail.com>
2016-03-26 04:15:05 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
- position = position.to_i
-  if position_taken?(board, position - 1) || !(position.between?(1, 9))
+  if position_taken?(board, position.to_i - 1) || !(position.to_i.between?(1, 9))
     false
   else
     true
