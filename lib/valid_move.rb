b0fb2a4f1aac597d1239059a1af3897cd707472f
dpreli <dave.preli@gmail.com>
2016-03-29 14:56:49 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,10 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, position)
-  if position.to_i-1.between?(0, 8) == true
-    position_taken(board, position)
+  if (position.to_i-1).between?(0, 8) == true && position_taken?(board, position) == false
+    true
+  else
+    false
   end
 end
 
