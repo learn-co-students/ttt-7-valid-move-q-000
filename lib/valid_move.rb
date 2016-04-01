beb01eea370b6e932292d43577925dd49422e6bb
Lenn Hypolite <lenntriniman@hotmail.com>
2016-04-01 17:55:18 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,18 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
+#     board[(position) -1 ]=position.gets.to_i
+    if position_taken?(board, position) == false
+      return true
+      elsif position_taken?(board, position) == true || board[position]!=(1..9).to_i
+      return false
+
 #     board[(position.to_i) -1 ]=position.gets
 #      if position_taken?(board,position)
 
 
 # #     elsif board[position].between?(1,10) && board[position]!=""
 # #     return false
-
+    end
 end
 
 
