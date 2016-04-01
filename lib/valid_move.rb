e0a64e5338c710b215f57caf04d2357925ffa9b1
jroxbury <roxbury.joseph@gmail.com>
2016-04-01 00:59:57 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
-
+def valid_move?(arr,pos)
+  pos = pos.to_i
+  pos = pos - 1
+  if position_taken?(arr,pos) == false && pos < 9 && pos >= 0
+    return true
+  else
+    return nil
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(bo,p)
+  if bo[p] == " " || bo[p] == "" || bo[p] == nil
+    return false
+  else
+    return true
+  end
+end
+
