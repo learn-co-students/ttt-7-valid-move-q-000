df7b964fabbe98de7e6ad80d6bf3744b0887814d
ptcloutier <cloutier.perrin@gmail.com>
2016-04-01 18:53:00 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,24 @@
-def valid_move?(board,position)
-    if !position_taken?[board, position.to_i] && [position.to_i-1].between?(0,9)
-   return true
-    else position_taken?[board, position.to_i]
-      return false
+
+def valid_move?(board, position)
+
+ position.to_i.between?(1,9) &&  !position_taken?(board, position.to_i-1)
 end
+
+
+#def valid_move?(board, position)
+ #position = position.to_i
+ #position = position-1
+ #if position >8  ||  position <0 || position_taken?(board,position)
+  # return false
+  # else
+ #  return true
+#end
+#end
+
+ def position_taken?(board, position)
+  if board[position] == "X"|| board[position] == "O"
+  return true
+  elsif board[position] == " " || board[position]== "" || board[position] == nil
+  return false
+  end
 end
