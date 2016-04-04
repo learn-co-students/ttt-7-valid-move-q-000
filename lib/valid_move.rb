6c25c005376d7fcedafe22d06eebab07834c6bf8
kode-tiki <upp_mike@yahoo.com>
2016-04-04 05:18:59 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,11 +6,9 @@ def valid_move?(board, position)
     return false
   end
 
-
   return !position_taken?(board, position)
 
 end
-
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
