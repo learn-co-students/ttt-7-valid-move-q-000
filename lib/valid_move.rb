20842c45f448b8868c49e6d5f535bce3ee1d6327
chcculle <cathy@softwareoptions.com>
2016-04-06 15:57:38 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(board, pos)
+  pos = pos.to_i
+  pos >=0 && (pos < board.size) && !(position_taken? board, pos)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? board, pos
+   (board[pos-1].nil?  || board[pos-1] == "" || board[pos-1] == " ") ? false : true
+end
