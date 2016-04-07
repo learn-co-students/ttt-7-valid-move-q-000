84aa34b9b773c54c0e9f34c17b8b27b604c40a52
Jenni Wise <wiselyjen@gmail.com>
2016-04-07 22:01:53 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
+ def position_taken?(board, position) #this wasn't working when defined after valid move :(
+    if board[position] == " " || board[position] == "" || board[position] == nil
+      false
+      else
+      true
+    end
+  end
+
+def valid_move?(board, position)
+   if position.to_i.between?(1,9) && position_taken?(board, position.to_i-1) == false
+    true
+   else
+    false
+  end
+
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -1,7 +1,12 @@
 require_relative "../lib/valid_move.rb"
 
-describe './lib/valid_move.rb' do
-  # Needs more specs
+describe '#valid_move? in ./lib/valid_move.rb ' do
+  it 'accepts a board and the position to check as arguments' do
+    board = []
+    position = 0
+
+    expect{valid_move?(board, position)}.to_not raise_error
+  end
 
   it 'returns true for a valid position on an empty board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
@@ -12,11 +17,11 @@ describe './lib/valid_move.rb' do
   it 'returns nil or false for an occupied position' do
     board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
     position = "5"
-    
+
     expect(valid_move?(board, position)).to be_falsey
   end
 
-  it 'returns nil or false for a position that is not on the board' do 
+  it 'returns nil or false for a position that is not on the board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     position = 100
 
