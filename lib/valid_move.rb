caf80bf604a29be49ac189c4882520a86e23075b
Lenn Hypolite <lenntriniman@hotmail.com>
2016-04-11 20:32:10 +0000

    nitrous-backup at Mon Apr 11 20:32:10 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
 # code your #valid_move? method here
+def position_taken?(board, position)
+  if board[position] == "" || board[position] == " " || board[position] == nil
+      return false
+   elsif board[position] == "X" || board[position] == "O"
+      return true
+  end
+end
 def valid_move?(board, position)
 #     board[(position) -1 ]=board[position].gets.to_i
     if position_taken?(board, position) #== false || board[position].between(1,9)
