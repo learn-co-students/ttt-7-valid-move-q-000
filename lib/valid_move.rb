4124a4d3e3275eb49567883ca0b3231db36b0277
beccagotch <becca.gotch@archiv8.com>
2016-04-17 01:35:41 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,8 @@
 #code your #valid_move? method here
-def valid_move? (board, position)
-  if (position.to_i-1).between?(0,8) && !position_taken?(board, position)
-    true
-  elsif !(position.to_i-1).between?(0,8) || position_taken?(board, position)
-    false
-  end
+def valid_move?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
+
 
 
 
