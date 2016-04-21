c598479579ce1c256e864961b6c2ce471e1c1ace
CodeAlyx <alyxanderthibodeaux@gmail.com>
2016-04-21 13:41:52 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
-# code your #valid_move? method here
+def valid_move?(board, position)
+  if position.to_i.between?(1, 9)
+    if !position_taken?(board, position.to_i - 1)
+      return true
+    else
+      puts "That position is already taken. Sorry."
+      return false
+    end
+  else
+    puts "That is not a valid board space. Try again."
+    return false
+  end
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  if board[position] == "X" || board[position] == "O"
+    return true
+  else
+    return false
+  end
+end
