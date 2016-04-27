dad28932e0be61c28c1afc47ee555782ccd18655
codmo <mkot29@gmail.com>
2016-04-26 19:25:29 -0700

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,31 @@
-# code your #valid_move? method here
+def position_taken?(board, position)
+  if board[position] == " "
+    false
+  elsif board[position] == ""
+    false
+  elsif board[position] == nil
+    false
+  elsif board[position] == "X"
+    true
+  elsif board[position] == "O"
+    true
+  end
+end
+
+def within_board?(board, position)
+  if board[position] < 0 && board[position] > 8
+    false
+  else board[position] >= 0 && board[position] <= 8
+    true
+  end
+end
+
+def valid_move?(board, position)
+  if !position_taken? && within_board?
+    true
+  else
+    false
+  end
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
