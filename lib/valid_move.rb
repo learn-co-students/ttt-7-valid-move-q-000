52468f0825953467547a0a1e8eb8c443bfc0c7ac
Mike Hickman <mike@pdxmike.com>
2016-04-29 00:55:16 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
 def valid_move?(board, position)
   position = position.to_i
   position.between?(1, 9) && position_taken?(board, position-1) == false ? true : false
-
-
 end
 
 def position_taken?(board, position)
