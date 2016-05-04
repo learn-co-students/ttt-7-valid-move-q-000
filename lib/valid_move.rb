ec095254d9e637370aa4bac25f642a0fcda6a610
m-maste <mitchell.masterson@gmail.com>
2016-05-04 19:25:17 +0000

    nitrous-backup at Wed May  4 19:25:17 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,5 +5,5 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
-  board[position] != " "
+  board[position] != " " && board[position] != "" && board[postion] != nil
 end
