74d9cb274f3b782b8e9787fb8b99868c1f9cabd9
achasveachas <chilikkal@gmail.com>
2016-05-06 01:40:10 +0000

    nitrous-backup at Fri May  6 01:40:10 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def valid_move?(board, position)
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
     return false    
