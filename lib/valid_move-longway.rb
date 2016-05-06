049832eaf64fc666c1bac45150fd8137e04d7251
David Holton <it.arcanum@gmail.com>
2016-05-05 22:08:01 -0400

    done

--- lib/valid_move-longway.rb
+++ lib/valid_move-longway.rb
@@ -1,8 +1,12 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
 	position=position.to_i - 1
-	if position.between?(0,8) && position_taken?(board, position)
-		return true
+	if position == 0 || position.between?(1,8)
+		if position_taken?(board, position)
+			return true
+		else !position_taken?(board, position)
+			return false
+		end
 	else
 		return false
 	end
