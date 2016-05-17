3424e5d8ee2d4e74f980d3d231614f6eb6d790a0
Katarzyna Kisiel <kate.kisiel@gmail.com>
2016-05-16 23:09:31 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,5 +10,5 @@ end
 
 
 def valid_move?(board, position)
-  !position_taken?(board, position) && position.between?(1, 9)
+  !position_taken?(board, position.to_i-1) && position.to_i.between?(1, 9)
 end
