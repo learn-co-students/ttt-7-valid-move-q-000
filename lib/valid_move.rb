c3c48dcf7867ca790791926a39516e89c5f6e57c
David O'Dea <d.odea4@nuigalway.ie>
2016-05-18 20:46:08 +0000

    nitrous-backup at Wed May 18 20:46:07 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 def valid_move?(board, position)
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-end
+  if position.to_i.between?(1,9)
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
+  end
 def position_taken?(board, location)
    board[location] != " "
  end
