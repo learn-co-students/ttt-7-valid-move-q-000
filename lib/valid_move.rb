47c5f32056fe51a2154cc166d81d8ed69a4d2b85
Malind Nikulski <malind.nikulski@gmail.com>
2016-05-20 10:10:59 -0600

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,14 @@
-def position_taken?(board, index)
-  if board[index.to_i-1] == " " || board[index.to_i-1] == ""
+def valid_move?(board, position)
+  index = position.to_i-1
+  if index.between?(0,8) && position_taken?(board, index)
     true
   else
     false
   end
 end
 
-def valid_move?(board, position)
-  index = position.to_i - 1
-  if index.between?(0,8) && !position_taken?(board, index.to_i-1)
+def position_taken?(board, index)
+  if board[index.to_i-1] == "X" || board[index.to_i-1] == "O"
     true
   else
     false
