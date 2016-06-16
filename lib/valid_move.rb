822a87f018ed9a2c50a28e5f3e063fc87c9027a5
Errol Rodrigues <errolrodrigues@hotmail.co.uk>
2016-06-16 12:04:02 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
 
 
+def valid_move?(board,input)
+  user_input = input.to_i
+  index = user_input - 1
+  move = ""
+
+  if !(index >= 0 && index <= 8) && board[index] == " " || board[index] == ""
+    move = true
+  elsif !(index >= 0 && index <= 8) && board[index] == "X" || board[index] == "O"
+    move = false
+
+  end
+end
+
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
