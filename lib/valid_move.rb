a61aaa348224e4fedd28d464c0a6193b8a57035d
Alicia Stillwelll <alicia.stillwell@gmail.com>
2016-07-20 22:09:48 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move? (board, idx)
+  return (idx < 10 && idx >= 0) && !position_taken?(board, idx)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, idx)
+  return (board[idx] == "X" || board[idx] == "O")
+end
