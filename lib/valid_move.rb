b6ef0dd7b9ff757e7439f853391ab4b7be839eb9
Steven Tang <5teven7ang@gmail.com>
2016-07-20 08:18:28 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
+require_relative '../../ttt-6-position-taken-rb-q-000/lib/position_taken.rb'
+
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+if index.between?(0,8) && !position_taken?(board, index)
+  return true
+else return false
+end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
