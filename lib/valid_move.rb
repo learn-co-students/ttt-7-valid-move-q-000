f91f17ceaeb09f7d15311dc2adeb7f2e37f97545
mo6709 <mo6709@gmail.com>
2016-08-02 15:06:07 -0400

    no need for false, nil will suffice

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-	if !(position_taken?(board, index)) && (1..board.length).cover?(index + 1)
-       true	
-    else
-      false	
+	if !(position_taken?(board, index)) && (1..board.length).cover?(index + 1) 
+		true
 	end
-	
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
