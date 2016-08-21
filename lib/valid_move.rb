e50963d77e2f96e0b9ef21d67b2d07fce30157dd
Julie Anne McManus <julieannesays@gmail.com>
2016-08-21 20:42:29 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,10 @@ def valid_move?(board,position)
   position.between?(0,8) && !position_taken?(board,position)
 end
 
-def position_taken?(board,position)
-  if (board[position]) == [" ", "", nil]
+def position_taken?(board, position)
+  if [" ", "", nil].include?(board[position])
     return false
-  else (board[position]) == ["X", "O"]
+  else ["X", "O"].include?(board[position])
     return true
   end
 end
