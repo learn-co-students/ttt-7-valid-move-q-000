2603ffa4c9f0552acf81e79269c6e0f507b454e0
cyanocobalamin <orenjuice@yahoo.com>
2017-08-11 02:06:47 -0400

    done

---
+++ lib/valid_move?.rb
@@ -0,0 +1,10 @@
+require_relative '../ttt-6-position-taken-rb-q-000/lib/position_taken.rb'
+
+def valid_move? (board, index)
+  if index.between(0,8) && !position_taken(board, index)
+    return true
+  else
+    return false
+  end
+end
+  
