e780860c2b2e5d446ec8bc7062b8914c6283b220
cyanocobalamin <orenjuice@yahoo.com>
2017-08-11 16:26:03 -0400

    Success.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
-# code your #valid_move? method here
+require_relative '../../ttt-6-position-taken-rb-q-000/lib/position_taken.rb'
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board,index)
+  if index.between?(0,8) && !position_taken?(board,index) 
+    return true
+  else
+    return false
+end    
+end
