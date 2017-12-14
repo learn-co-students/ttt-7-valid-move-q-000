cd56ec26b099ee9a9baa2766d67c57ec2f9b687f
Amy Carney <amy.carney@alaska.gov>
2017-12-14 18:40:08 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
-
+def valid_move?
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
