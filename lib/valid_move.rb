36458bf557db73e6a47fd2676628e03d55ad9227
Kalil Salkey <ksalkey@gmail.com>
2019-01-02 20:47:37 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,9 @@
 def valid_move?(board, index)
  if board[index] == "X" || board[index] == "O"&& index.between?(0, 8)
     return false 
-    
   elsif 
     board[index] == " "||board[index] == ""|| board[index] == nil && index.between?(0, 8)
     return true 
-    
   else 
     return false
     
